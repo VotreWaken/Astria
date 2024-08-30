0bdb2273cc65179271f3ca0b4228b2b87ffe9f8e
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-30 23:50:21 +0300

    Define the IQueryRepository<T> interface for querying entities in the repository.

---
+++ Astria.QueryRepository/Repository/IQueryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astria.QueryRepository.Repository
+{
+	public interface IQueryRepository<T>
+	{
+		Task<IEnumerable<T>> FindAllAsync();
+		Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> predicate);
+		Task<T> FindByIdAsync(Guid id);
+	}
+}

c3ad2a3961d464b63cd996da89b2f19fc9fb9394
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-30 18:34:25 +0300

    implement IEventSourcingClient interface that defines the contract for interacting with event sourcing clients

---
+++ Astria.EventSourcingRepository/Client/IEventSourcingClient.cs
@@ -0,0 +1,10 @@
+﻿using Astria.SharedKernel;
+
+namespace Astria.EventSourcingRepository.Client
+{
+	public interface IEventSourcingClient
+	{
+		Task<(long Version, IEnumerable<IDomainEvent> Events)> ReadEventsAsync(Guid aggregateId);
+		Task AppendEventAsync(IDomainEvent @event);
+	}
+}

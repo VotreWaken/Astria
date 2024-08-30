b542a6a97b52606ac92f16d786738a76b61d5f53
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-30 23:15:29 +0300

    Create the ProductCreatedEvent class to represent the event triggered when a product is created. Also, implement the ProductCreatedConsumer to handle this event.

---
+++ Astria.Rabbitmq/Events/ProductCreatedEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astria.Rabbitmq.Events
+{
+	public class ProductCreatedEvent
+	{
+		public Guid ProductId { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public decimal Price { get; set; }
+		public bool IsAvailable { get; set; }
+		public DateTime Date { get; set; }
+		public Guid ModelId { get; set; }
+		public Guid PreviewImageId { get; set; }
+	}
+}

baf2ad1f5bc524ae7e4b388d90fe3e46ce31b226
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-31 00:04:10 +0300

    Create specific exception classes inheriting from AuthenticationException

---
+++ Astria.AuthenticationManagement/AuthenticationManagement.Authentication/Exceptions/RoleCannotBeAssignedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationManagement.Authentication.Exceptions
+{
+	class RoleCannotBeAssignedException : AuthenticationException
+	{
+		public RoleCannotBeAssignedException(string email) : base($"Idenity Role cannot be assigned to: {email}") { }
+		public RoleCannotBeAssignedException(Guid userId) : base($"Idenity Role cannot be assigned to: {userId}") { }
+	}
+}
---
+++ Astria.AuthenticationManagement/AuthenticationManagement.Authentication/Exceptions/UserCannotBeCreatedException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationManagement.Authentication.Exceptions
+{
+	class UserCannotBeCreatedException : AuthenticationException
+	{
+		public UserCannotBeCreatedException(string email) : base($"Idenity User cannot be created: {email}") { }
+	}
+}

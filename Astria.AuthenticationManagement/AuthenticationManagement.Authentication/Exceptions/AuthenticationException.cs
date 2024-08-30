95e183101e9581ba1ee8298746ec20a48796ac85
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-31 00:03:29 +0300

    Create a base exception class for handling authentication-related errors

---
+++ Astria.AuthenticationManagement/AuthenticationManagement.Authentication/Exceptions/AuthenticationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationManagement.Authentication.Exceptions
+{
+	public class AuthenticationException : Exception
+	{
+		public AuthenticationException() { }
+		public AuthenticationException(string message) : base(message) { }
+		public AuthenticationException(string message, Exception inner) : base(message, inner) { }
+	}
+}

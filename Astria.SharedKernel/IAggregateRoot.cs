626e8f5a51a9fc711780b517109f19ba8799b2d8
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-30 22:21:20 +0300

    Create the IAggregateRoot interface to define the contract for aggregate roots in the domain model

---
+++ Astria.SharedKernel/IAggregateRoot.cs
@@ -0,0 +1,14 @@
+﻿namespace Astria.SharedKernel
+{
+	public interface IAggregateRoot
+	{
+		public Guid AggregateId { get; }
+		public long Version { get; }
+
+		public void LoadFromHistory(long version, IEnumerable<IDomainEvent> history);
+
+		public IEnumerable<IDomainEvent> GetUncommittedChanges();
+
+		public void MarkChangesAsCommitted();
+	}
+}

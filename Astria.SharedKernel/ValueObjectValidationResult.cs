2a9c2da62b9fb365e15235476b142a291fa816f0
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-30 22:23:29 +0300

    Define the ValueObjectValidationResult class to encapsulate the result of value object validation, including the value object and any business errors.

---
+++ Astria.SharedKernel/ValueObjectValidationResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Astria.SharedKernel
+{
+	public class ValueObjectValidationResult
+	{
+		public IValueObject ValueObject { get; private set; }
+		public List<string> BusinessErrors { get; private set; }
+
+		public ValueObjectValidationResult(IValueObject valueObject, List<string> businessErrors)
+		{
+			ValueObject = valueObject;
+			BusinessErrors = businessErrors;
+		}
+	}
+}

1b22680eab5f4ac8093180de0e59094d40860948
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-30 23:49:47 +0300

    Define the IProjectionRepository<T> interface for operations that modify entities in the repository.

---
+++ Astria.QueryRepository/Repository/IProjectionRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astria.QueryRepository.Repository
+{
+	public interface IProjectionRepository<T> : IQueryRepository<T>
+	{
+		Task InsertAsync(T entity);
+		Task UpdateAsync(T entity);
+		Task DeleteAsync(Guid Id);
+	}
+}

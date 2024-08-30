4272427ec4e50a2635e85d645c93c4a91a82966a
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-31 00:06:27 +0300

    Define configuration classes for JWT settings and database connections

---
+++ Astria.AuthenticationManagement/AuthenticationManagement.Authentication/Configuration/JwtSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationManagement.Authentication.Configuration
+{
+	public class JwtSettings
+	{
+		public string Issuer { get; set; }
+		public string Secret { get; set; }
+		public int ExpirationInMinutes { get; set; }
+	}
+}
---
+++ Astria.AuthenticationManagement/AuthenticationManagement.Authentication/Configuration/MySqlSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationManagement.Authentication.Configuration
+{
+	public class MySqlSettings
+	{
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public string Url { get; set; }
+		public int Port { get; set; }
+		public string Database { get; set; }
+	}
+}

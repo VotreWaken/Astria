fd8cdf9eb9598f38f78164330dab1e50af67af67
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-30 23:49:08 +0300

    Implement the QueryEntity class as an abstract base class for entities in MongoDB

---
+++ Astria.QueryRepository/Repository/QueryEntity.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Astria.QueryRepository.Repository
+{
+	public abstract class QueryEntity : IQueryEntity
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.String)]
+		public Guid Id { get; set; }
+		public long Version { get; set; }
+	}
+}

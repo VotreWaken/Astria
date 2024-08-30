bfc6dd912408232ee613295a1d29d5ce57fc0b38
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-30 18:22:43 +0300

    feat: add IEventSourcingRepository<TAggregate> interface that provides managing event-sourced aggregates

---
+++ Astria.EventSourcingRepository/Repository/IEventSourcingRepository.cs
@@ -0,0 +1,10 @@
+﻿using Astria.SharedKernel;
+
+namespace Astria.EventSourcingRepository.Repository
+{
+	public interface IEventSourcingRepository<TAggregate> where TAggregate : IAggregateRoot
+	{
+		Task<TAggregate> FindByIdAsync(Guid id);
+		Task SaveAsync(TAggregate aggregate);
+	}
+}

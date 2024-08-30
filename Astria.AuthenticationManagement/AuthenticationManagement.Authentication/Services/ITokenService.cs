3def30e2e14990f7f6a64128cfdda5685807b751
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-31 00:00:45 +0300

    Define the ITokenService interface that specifies the method for generating JWT tokens.

---
+++ Astria.AuthenticationManagement/AuthenticationManagement.Authentication/Services/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using AuthenticationManagement.Authentication.Entities;
+
+namespace AuthenticationManagement.Authentication.Services
+{
+	public interface ITokenService
+	{
+		string GenerateJwt(User user, IList<string> roles);
+	}
+}

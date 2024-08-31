4167b5136d207e3b859b4db2e3af0093bcd8680d
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-31 09:02:58 +0300

    Implement exception handling for domain events in both Admin and Customer aggregates

---
+++ Astria.AuthenticationManagement/AuthenticationManagement.Domain/BoundedContexts/UserAccountManagement/Exceptions/ConfirmationEmailNotEqualException.cs
@@ -0,0 +1,9 @@
+﻿using AuthenticationManagement.Domain.Exceptions;
+
+namespace AuthenticationManagement.Domain.BoundedContexts.UserAccountManagement.Exceptions
+{
+	public class ConfirmationEmailNotEqualException : DomainException
+	{
+		public ConfirmationEmailNotEqualException() : base("The Accounts existing email address does not match the provided confirmation email address.") { }
+	}
+}
---
+++ Astria.AuthenticationManagement/AuthenticationManagement.Domain/BoundedContexts/UserAccountManagement/Exceptions/UserAccountNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using AuthenticationManagement.Domain.Exceptions;
+
+namespace AuthenticationManagement.Domain.BoundedContexts.UserAccountManagement.Exceptions
+{
+	public class UserAccountNotFoundException : DomainException
+	{
+		public UserAccountNotFoundException() : base("User Account not found.") { }
+	}
+}

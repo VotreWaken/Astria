5548569f546b64382568ae9de73bd2ae9bc54c3f
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-31 10:22:44 +0300

    Implement UpdateEmailAddressDTO for email address updates

---
+++ Astria.AuthenticationManagement/AuthenticationManagement.API/DTOs/UpdateEmailAddressDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthenticationManagement.API.DTOs
+{
+	public class UpdateEmailAddressDTO
+	{
+		/*
+        public long ExpectedVersion { get; set; }
+        */
+
+		public string Email { get; set; }
+	}
+}

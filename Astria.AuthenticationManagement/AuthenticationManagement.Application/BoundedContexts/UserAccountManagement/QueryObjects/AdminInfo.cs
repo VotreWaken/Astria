7c40684d82cd3b32499386e51f27978f8af5218b
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-31 09:47:35 +0300

    Add AdminInfo class for managing query request for admin account details

---
+++ Astria.AuthenticationManagement/AuthenticationManagement.Application/BoundedContexts/UserAccountManagement/QueryObjects/AdminInfo.cs
@@ -0,0 +1,19 @@
+﻿using Astria.QueryRepository.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationManagement.Application.BoundedContexts.UserAccountManagement.QueryObjects
+{
+	public class AdminInfo : QueryEntity
+	{
+		public string Email { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string HashedPassword { get; set; }
+		public Guid AdminImageId { get; set; }
+
+	}
+}

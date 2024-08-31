ca8e70684f5db0528d52b2b2b2ff4c29ba905983
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-31 11:45:16 +0300

    Create an extension method for configuring the email service within the application

---
+++ Astria.AuthenticationManagement/AuthenticationManagement.API/Extensions/EmailServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Celestial.EmailService.Services;
+
+namespace AuthenticationManagement.API.Extensions
+{
+	public static class EmailServiceExtensions
+	{
+		public static IServiceCollection AddEmailService(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddScoped<IEmailService>(provider =>
+				new DemoEmailService(
+					provider.GetRequiredService<ILogger<DemoEmailService>>(),
+					provider.GetRequiredService<IHttpContextAccessor>(),
+					configuration
+				));
+
+			return services;
+		}
+	}
+}

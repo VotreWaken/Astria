a68769d7b9893d836a243477e1c18e7df8242053
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-31 09:04:09 +0300

    Implement and validate the EmailAddress, HashedPassword and UserFullName value objects

---
+++ Astria.AuthenticationManagement/AuthenticationManagement.Domain/BoundedContexts/UserAccountManagement/ValueObjects/HashedPassword.cs
@@ -0,0 +1,45 @@
+﻿using Astria.SharedKernel;
+
+namespace AuthenticationManagement.Domain.BoundedContexts.UserAccountManagement.ValueObjects
+{
+	public class HashedPassword : ValueObject
+	{
+		public string Value { get; private set; }
+
+		private HashedPassword()
+		{ }
+
+		private HashedPassword(string value)
+		{
+			Value = value;
+		}
+
+		public static ValueObjectValidationResult Create(string value)
+		{
+			List<string> businessLogicErrors = new();
+
+			if (value is null) businessLogicErrors.Add("Hashed Password is invalid.");
+
+			if (businessLogicErrors?.Any() == true)
+				return new ValueObjectValidationResult(null, businessLogicErrors);
+
+			return new ValueObjectValidationResult(new HashedPassword(value), null);
+		}
+
+		public bool EqualsCurrentPassword(HashedPassword toComparePasswort)
+			=> Value == toComparePasswort;
+
+		#region Conversion
+		public static implicit operator string(HashedPassword value)
+			=> value.Value;
+		#endregion
+
+
+		public override string ToString() => Value;
+
+		protected override IEnumerable<object> GetEqualityComponents()
+		{
+			yield return Value;
+		}
+	}
+}

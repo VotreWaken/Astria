ebbdf15609968ca8e9c206986ba65d1ddcfac3d6
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-31 09:54:00 +0300

    Added LoggingBehaviour pipeline to log requests and responses, including information about the user who made the request

---
+++ Astria.AuthenticationManagement/AuthenticationManagement.Application/Pipelines/LoggingBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AuthenticationManagement.Application.Pipelines
+{
+	public class LoggingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+	{
+		private readonly ILogger<LoggingBehaviour<TRequest, TResponse>> _logger;
+		private readonly IHttpContextAccessor _httpContextAccessor;
+		public LoggingBehaviour(ILogger<LoggingBehaviour<TRequest, TResponse>> logger, IHttpContextAccessor httpContextAccessor)
+		{
+			_logger = logger;
+			_httpContextAccessor = httpContextAccessor;
+		}
+		public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+		{
+			var currentUser = _httpContextAccessor.HttpContext?.User?.Identity?.Name;
+
+			_logger.LogInformation($"Handling {typeof(TRequest).Name} - requsted by {currentUser ?? "unknown"}");
+			var response = await next();
+
+			_logger.LogInformation($"Handled {typeof(TResponse).Name} - requsted by {currentUser ?? "unknown"}");
+			return response;
+		}
+	}
+}

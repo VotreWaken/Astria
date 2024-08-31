746a3b0b3cfabd055e62519667520109d6535240
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-31 09:53:16 +0300

    Introduced FailureTypes enum to categorize failure reasons in application logic

---
+++ Astria.AuthenticationManagement/AuthenticationManagement.Application/Results/FailureTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationManagement.Application.Results
+{
+	public enum FailureTypes
+	{
+		None,
+		BusinessRule,
+		NotFound,
+		Duplicate
+	}
+}

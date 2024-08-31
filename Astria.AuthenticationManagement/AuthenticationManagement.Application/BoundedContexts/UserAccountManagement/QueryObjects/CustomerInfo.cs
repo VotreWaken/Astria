dc4d877debf0769c58f540ce47b38f7357b66ae1
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-31 09:47:51 +0300

    Add CustomerInfo class for managing query request for customer account details

---
+++ Astria.AuthenticationManagement/AuthenticationManagement.Application/BoundedContexts/UserAccountManagement/QueryObjects/CustomerInfo.cs
@@ -0,0 +1,18 @@
+﻿using Astria.QueryRepository.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationManagement.Application.BoundedContexts.UserAccountManagement.QueryObjects
+{
+	public class CustomerInfo : QueryEntity
+	{
+		public string Email { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string HashedPassword { get; set; }
+		public Guid UserImageId { get; set; }
+	}
+}

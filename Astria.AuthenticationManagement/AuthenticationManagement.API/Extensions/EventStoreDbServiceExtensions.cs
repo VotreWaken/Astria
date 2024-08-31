838a12dfa13c403a05b6475fd269ba78081b4a58
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-31 11:45:31 +0300

    Implement an extension method to configure the Event Store database service

---
+++ Astria.AuthenticationManagement/AuthenticationManagement.API/Extensions/EventStoreDbServiceExtensions.cs
@@ -0,0 +1,39 @@
+﻿using EventStore.Client;
+using Astria.EventSourcingRepository.Client;
+using Astria.EventSourcingRepository.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Net.Security;
+
+namespace AuthenticationManagement.API.Extensions
+{
+	public static class EventStoreDbServiceExtensions
+	{
+		public static IServiceCollection AddEventStoreDbService(this IServiceCollection services, EventStoreDbSettings settings)
+		{
+
+			var eventStoreClientSettings = new EventStoreClientSettings
+			{
+				ConnectivitySettings =
+				{
+					Address = new Uri(settings.Uri)
+				},
+				DefaultCredentials = new UserCredentials(settings.Username, settings.Password),
+				CreateHttpMessageHandler = () =>
+				new SocketsHttpHandler
+				{
+					SslOptions = new SslClientAuthenticationOptions
+					{
+						RemoteCertificateValidationCallback = delegate { return true; }
+					}
+				}
+			};
+
+			services.AddSingleton(_ => new EventStoreClient(eventStoreClientSettings)).BuildServiceProvider();
+			services.AddScoped<IEventSourcingClient, EventStoreDbClient>();
+
+			return services;
+		}
+	}
+}

1996bb1d260aed0fa17bf9fa97281b6caa22fa65
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-08-31 09:27:46 +0300

    Implement the command to allow an admin to change their email address

---
+++ Astria.AuthenticationManagement/AuthenticationManagement.Application/BoundedContexts/UserAccountManagement/Commands/AdminAccountChangeEmailCommand.cs
@@ -0,0 +1,60 @@
+﻿using AuthenticationManagement.Application.Results;
+using AuthenticationManagement.Authentication.Repository;
+using AuthenticationManagement.Domain.BoundedContexts.UserAccountManagement.Aggregates;
+using AuthenticationManagement.Domain.Exceptions;
+using Astria.EventSourcingRepository.Repository;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AuthenticationManagement.Application.BoundedContexts.UserAccountManagement.Commands
+{
+	public class AdminAccountChangeEmailCommand : IRequest<CommandResult>
+	{
+		public long ExpectedVersion { get; set; }
+		public Guid UserId { get; set; }
+		public string NewEmail { get; set; }
+	}
+
+	public class AdminAccountChangeEmailCommandHandler : IRequestHandler<AdminAccountChangeEmailCommand, CommandResult>
+	{
+		private readonly IEventSourcingRepository<Admin> _repository;
+		private readonly IAuthenticationRepository _authRepository;
+
+		public AdminAccountChangeEmailCommandHandler(IEventSourcingRepository<Admin> repository, IAuthenticationRepository authRepository)
+		{
+			_repository = repository ?? throw new ArgumentNullException(nameof(repository));
+			_authRepository = authRepository ?? throw new ArgumentNullException(nameof(authRepository));
+		}
+
+		public async Task<CommandResult> Handle(AdminAccountChangeEmailCommand command, CancellationToken cancellationToken)
+		{
+			if (await _authRepository.EmailAddressInUse(command.NewEmail))
+				return CommandResult.EmailInUse(command.NewEmail);
+
+			try
+			{
+				var admin = await _repository.FindByIdAsync(command.UserId);
+				if (admin is null)
+					return CommandResult.NotFound(command.UserId);
+
+				/*
+                if (admin.Version != command.ExpectedVersion)
+                    return CommandResult.NotFound(command.UserId);
+                */
+				admin.ChangeEmail(command.NewEmail);
+
+				await _authRepository.ChangeEmail(command.UserId, command.NewEmail);
+
+				await _repository.SaveAsync(admin);
+
+				return CommandResult.Success();
+			}
+			catch (DomainException ex)
+			{
+				return CommandResult.BusinessFail(ex.Message);
+			}
+		}
+	}
+}

e1362f43e3abc9b65291fcfcaaa4b4939fbaaa82
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-09-17 20:35:33 +0300

    feat(domain): add Model aggregate with event handling

---
+++ Astria.ModelManagement/ModelsManagment.Domain/BoundedContexts/ModelsManagment/Events/ModelDeletedEvent.cs
@@ -0,0 +1,20 @@
+﻿using Astria.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsManagment.Domain.BoundedContexts.ModelsManagment.Events
+{
+	public class ModelDeletedEvent : DomainEvent
+	{
+		public Guid Id { get; private set; }
+
+		public ModelDeletedEvent(Guid aggregateId, Guid id)
+			: base(aggregateId)
+		{
+			Id = id;
+		}
+	}
+}

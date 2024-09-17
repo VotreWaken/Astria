c106cc10c0534781a25c21766c4878dd7d1bfbcd
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-09-17 20:46:28 +0300

    feat(infra): implement ModelDbContext

---
+++ Astria.ModelManagement/ModelsManagment.Infrastructure/DataContext/ModelDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModelsManagment.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsManagment.Infrastructure.DataContext
+{
+	public class ModelDbContext : DbContext
+	{
+		public ModelDbContext(DbContextOptions<ModelDbContext> options) : base(options)
+		{ }
+
+		public DbSet<Model> Models { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Model>().HasKey(o => o.ModelId);
+		}
+	}
+}

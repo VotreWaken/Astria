d3d67d9219494b3020b0f209c6181e2e1f56f567
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-09-17 20:46:56 +0300

    feat(infra): implement Model repository

---
+++ Astria.ModelManagement/ModelsManagment.Infrastructure/Repositories/IModelRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelsManagment.Infrastructure.Entities;
+
+namespace ModelsManagment.Infrastructure.Repositories
+{
+	public interface IModelRepository
+	{
+		Task CreateModel(Model order);
+		Task<Model> GetModelById(Guid orderId);
+		Task<IEnumerable<Model>> GetModelsByProductId(Guid userId);
+		Task UpdateModel(Model order);
+		Task DeleteModel(Guid orderId);
+	}
+}
